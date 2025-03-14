810f2be965f34833e7114b501129f8865044501f
Lucy Purinton <lucy.purinton@kellogg.northwestern.edu>
2025-03-14 20:41:38 +0000

    have to login to see posts

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,4 +1,5 @@
 class PlacesController < ApplicationController
+before_action :require_login
 
   def index
     @places = Place.all
@@ -6,7 +7,7 @@ class PlacesController < ApplicationController
 
   def show
     @place = Place.find_by({ "id" => params["id"] })
-    @entries = Entry.where({ "place_id" => @place["id"] })
+    @entries = Entry.where({ "place_id" => @place["id"], "user_id" => session["user_id"] })
   end
 
   def new
@@ -18,5 +19,12 @@ class PlacesController < ApplicationController
     @place.save
     redirect_to "/places"
   end
+  private
 
+  def require_login
+    unless session[:user_id]
+      flash[:alert] = "You must be logged in to access this page."
+      redirect_to login_path  # ✅ Redirect to login if not logged in
+    end
+  end
 end
